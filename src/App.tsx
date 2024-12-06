cb38ddc84ca91bd235c81f36d857a1e9ff880793
Jakub Bajkowski <k.bajkowski7@wp.pl>
2024-12-06 07:54:04 +0100

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ export class App extends React.Component<{}, State> {
               onClick={() => {
                 this.removeHandler();
               }}
-            />
+            ></button>
           </h1>
         ) : (
           <h1 className="title">No goods selected</h1>
@@ -57,48 +57,46 @@ export class App extends React.Component<{}, State> {
 
         <table className="table">
           <tbody>
-            {goods.map(good => {
-              return (
-                <tr
-                  data-cy="Good"
-                  key={good}
-                  className={
-                    selectedGood === good
-                      ? 'has-background-success-light'
-                      : undefined
-                  }
-                >
-                  <td>
-                    {selectedGood !== good ? (
-                      <button
-                        data-cy="AddButton"
-                        type="button"
-                        className="button"
-                        onClick={() => {
-                          this.goodHandler(good);
-                        }}
-                      >
-                        +
-                      </button>
-                    ) : (
-                      <button
-                        data-cy="RemoveButton"
-                        type="button"
-                        className="button is-info"
-                        onClick={() => {
-                          this.removeHandler();
-                        }}
-                      >
-                        -
-                      </button>
-                    )}
-                  </td>
-                  <td data-cy="GoodTitle" className="is-vcentered">
-                    {good}
-                  </td>
-                </tr>
-              );
-            })}
+            {goods.map(good => (
+              <tr
+                data-cy="Good"
+                key={good}
+                className={
+                  selectedGood === good
+                    ? 'has-background-success-light'
+                    : undefined
+                }
+              >
+                <td>
+                  {selectedGood !== good ? (
+                    <button
+                      data-cy="AddButton"
+                      type="button"
+                      className="button"
+                      onClick={() => {
+                        this.goodHandler(good);
+                      }}
+                    >
+                      +
+                    </button>
+                  ) : (
+                    <button
+                      data-cy="RemoveButton"
+                      type="button"
+                      className="button is-info"
+                      onClick={() => {
+                        this.removeHandler();
+                      }}
+                    >
+                      -
+                    </button>
+                  )}
+                </td>
+                <td data-cy="GoodTitle" className="is-vcentered">
+                  {good}
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </main>
